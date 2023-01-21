93e8291c8b942bfadae562329f40c6be8bf2dac6
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-21 11:41:55 -0400

    9.1 5-06-Construir el listado de Pokémons favoritos

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { getPokemons } from '@/utils';
 import { Layout } from '@/components/layout';
 import { NoFavorites } from '@/components/ui';
+import { Card, Grid } from '@nextui-org/react';
 
 export default function FavoritesPage() {
   const [favoritesPokemos, setFavoritesPokemos] = useState<number[]>([]);
@@ -14,7 +15,23 @@ export default function FavoritesPage() {
   return (
     <>
       <Layout title='Pokemons Favoritos'>
-        <NoFavorites />
+        {!favoritesPokemos.length ? (
+          <NoFavorites />
+        ) : (
+          <Grid.Container gap={2} direction='row' justify='flex-start'>
+            {favoritesPokemos.map((id) => (
+              <Grid key={id} xs={6} sm={3} md={2} xl={1}>
+                <Card isHoverable isPressable css={{ padding: 10 }}>
+                  <Card.Image
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                    width={'100%'}
+                    height={140}
+                  />
+                </Card>
+              </Grid>
+            ))}
+          </Grid.Container>
+        )}
       </Layout>
     </>
   );
