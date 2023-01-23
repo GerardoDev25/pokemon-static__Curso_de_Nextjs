dce4bf023305fbec3eda8a8f469f699727b0885f
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-23 13:04:47 -0400

    16. Open Graph Meta Tags

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
+
 import { Navbar } from '../ui';
 
 type Props = React.FC<{ children: React.ReactNode; title?: string }>;
+
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
 
 export const Layout: Props = ({ children, title }) => {
   return (
@@ -11,6 +14,9 @@ export const Layout: Props = ({ children, title }) => {
         <meta name='author' content='Gerardo Miranda' />
         <meta name='description' content={`informacion sobre el pokemon ${title}`} />
         <meta name='keyworks' content={`${title} pokemon, pokedex`} />
+        <meta property='og:title' content={`informacion de ${title}`} />
+        <meta property='og:description' content={`esta es la pagina de ${title}`} />
+        <meta property='og:image' content={`${origin}/img/banner.png`} />
       </Head>
       <Navbar />
       <main
